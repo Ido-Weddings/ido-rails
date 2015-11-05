60eef7377d2bdf68807e653d58ef21f908d4c266
wsickert <wesley.sickert@gmail.com>
2015-11-05 17:28:46 -0200

    edit protected forgery to accept json post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  skip_before_action :verify_authenticity_token, if: :json_request?
   protect_from_forgery with: :exception
+
+  protected
+  def json_request?
+    request.format.json?
+  end
 end
